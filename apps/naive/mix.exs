951efea8b329a352cdb3bca20bc12c06d771d557
Matheus Buss <bussmatheus@gmail.com>
2022-10-04 23:35:08 -0300

    Updated dependencies

--- apps/naive/mix.exs
+++ apps/naive/mix.exs
@@ -33,9 +33,7 @@ defmodule Naive.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:binance,
-       git: "https://github.com/cambiatus/binance.ex",
-       ref: "667c6672022b5a9370b539f0ee64755cbb131d32"},
+      {:binance, git: "https://github.com/cambiatus/binance.ex"},
       {:strategies, in_umbrella: true},
       {:binance_mock, in_umbrella: true},
       {:core, in_umbrella: true},
--- apps/strategies/mix.exs
+++ apps/strategies/mix.exs
@@ -27,7 +27,9 @@ defmodule Strategies.MixProject do
   defp deps do
     [
       {:erlport, "~> 0.10.1"},
-      {:poolboy, "~> 1.5"}
+      {:poolboy, "~> 1.5"},
+      {:core, in_umbrella: true},
+      {:decimal, "~> 2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
